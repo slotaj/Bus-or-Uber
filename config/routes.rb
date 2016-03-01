06c8898b738dd139736bc0402f6430ccf0c8b018
Justin Pease <justinpease2@gmail.com>
2016-03-01 15:01:02 -0700

    adds oauth for uber - must run in incognito mode to bypass ssl

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
+  get 'logout', to: 'sessions#destroy'
 
   get '/auth/uber', as: :login
   get "/auth/uber/callback", to: "sessions#create"
 
-  get 'dashboard', to: 'user#show'
+  get 'dashboard', to: 'users#show'
 end
