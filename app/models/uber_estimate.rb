04638e0e5879a0b238571d95de2164c252ee2805
Justin Pease <justinpease2@gmail.com>
2016-03-06 19:43:39 -0700

    cleans up the initializing of the uber estimates object

--- app/models/uber_estimate.rb
+++ app/models/uber_estimate.rb
@@ -6,13 +6,8 @@ class UberEstimate
   attr_reader :ride_estimates, :uber_arrival_estimates
 
   def initialize(ride_estimates, uber_arrival_estimates)
-    @ride_estimates = ride_estimates.prices.map do |ride|
-      ride['duration'] = trip_duration(ride['duration'])
-      build_object(ride)
-    end
-    @uber_arrival_estimates = uber_arrival_estimates.map do |estimate|
-      build_object(estimate)
-    end
+    @ride_estimates             = sanitize_ride_estimates(ride_estimates)
+    @uber_arrival_estimates     = sanitize_arrival_estimates(uber_arrival_estimates)
   end
 
   def self.create(start_lat, start_lng, end_lat, end_lng)
@@ -20,8 +15,23 @@ class UberEstimate
     uber_arrival_estimates = UberService.new.time_estimate(start_lat, start_lng)
     @info = new(ride_estimates, uber_arrival_estimates)
   end
+
   def self.estimate
     @info
+  end
+
+  def sanitize_ride_estimates(ride_estimates) # this is crutial!!!! this method goes into the hash and the 'prices' key to get the right info
+    ride_estimates.prices.map do |ride|
+      ride['duration'] = trip_duration(ride['duration'])
+      build_object(ride)
+    end
+  end
+
+  def sanitize_arrival_estimates(uber_arrival_estimates)
+    uber_arrival_estimates.map do |uber_estimate|
+      uber_estimate['estimate'] = trip_duration(uber_estimate['estimate'])
+      build_object(estimate)
+    end
   end
 
   def trip_duration(trip_duration)
